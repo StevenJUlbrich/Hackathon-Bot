ce1f443dca90ac79efeabd4983246591da91c5e4
Steven <stevenulbrich@me.com>
2018-06-28 10:20:27 -0500

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=gsupport-bot-Web-Deploy.pubxml -p:Password=fcbfSuGgeyq1ezSGgMoomEnNlA2oNwa76GbDwS4jz2bhg3hbwSKvcJBwfMiw
+
